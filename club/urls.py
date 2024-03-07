e7d221b6fac8cf43cdf2aa232d82f11a653a51f6
Mohamed Elbahy <ixbahy@gmail.com>
2024-03-07 16:47:37 +0200

    add: some of the club basis

--- club/urls.py
+++ club/urls.py
@@ -1,5 +1,11 @@
 from django.urls import  path
 import club.views as views
 urlpatterns=[
-
+    # path('/', views.ClubsView.as_view()),#should be searchable by a (name,category,rating,type) param
+    # path('/<int:pk>/', views.ClubView.as_view()),
+    # path('/roadmap/', views.RoadmapView.as_view()),
+    # path('/members/', views.ClubMembersView.as_view()),#should be searchable by a (name) param
+    # path('/reviews', views.ClubReviewsView.as_view()),
+    # path('/document', views.ClubDocumentView.as_view()),
+    # path('/instructor', views.ClubInstructorView.as_view()),
 ]
