01d3473b6a0418b2f3b34079d14cac016ed408b0
Mohamed Elbahy <ixbahy@gmail.com>
2024-03-18 17:31:50 +0200

    add swagger generated api docs

--- lore_services/urls.py
+++ lore_services/urls.py
@@ -17,10 +17,25 @@ Including another URLconf
 from django.contrib import admin
 from rest_framework import routers
 from django.urls import  path, include
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+# from rest_framework import permissions
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Lore APIs",
+      default_version='v1',
+      description="Lore services APIs",
+   ),
+   public=True,
+#    permission_classes=(permissions.AllowAny,),
+)
 
 router = routers.DefaultRouter()
 
 urlpatterns = [
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('admin/', admin.site.urls),
     path('', include('utils.urls')),
     path('club/', include('club.urls')),
