2e27e35c79c78805f57c63bd191f79cd5513ec9a
Mohamed Elbahy <ixbahy@gmail.com>
2024-03-29 01:21:15 +0200

    making my life easier

--- lore_services/settings.py
+++ lore_services/settings.py
@@ -149,10 +149,10 @@ CORS_ORIGIN_ALLOW_ALL=True
 
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=180),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=10),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=30),
-    "ROTATE_REFRESH_TOKENS": True,
-    "BLACKLIST_AFTER_ROTATION": True,
+    "ROTATE_REFRESH_TOKENS": False,
+    "BLACKLIST_AFTER_ROTATION": False,
     "UPDATE_LAST_LOGIN": False,
 
     "ALGORITHM": "HS256",
